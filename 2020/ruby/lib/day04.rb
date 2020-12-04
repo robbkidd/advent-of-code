f27605a0441855145b46103d03b7bf4ca1d4e01e
Robb Kidd <robb@thekidds.org>
2020-12-04 07:37:38 -0500

    2020 Day 4 Part 2
    Today's lesson: double-check that you're actually using the test data
    you think you're using.

--- 2020/ruby/lib/day04.rb
+++ 2020/ruby/lib/day04.rb
@@ -5,10 +5,15 @@ class Day04
   end
 
   def self.part1
-    Passport.scanner(load_passports).map(&:valid?).count(true)
+    Passport.scanner(load_passports)
+            .map(&:all_required_fields_present?)
+            .count(true)
   end
 
   def self.part2
+    Passport.scanner(load_passports)
+            .map(&:valid?)
+            .count(true)
   end
 
   def self.load_passports
@@ -17,8 +22,27 @@ class Day04
 end
 
 class Passport
-  REQUIRED_FIELDS = %w{ byr iyr eyr hgt hcl ecl pid cid }
-  OUR_HACK = REQUIRED_FIELDS - %w{ cid }
+
+  FIELD_RULES = {
+    # "cid" is not required in our hack
+    "byr" => -> (birth_year) { 1920 <= birth_year.to_i && birth_year.to_i <= 2002 },
+    "iyr" => -> (issue_year) { 2010 <= issue_year.to_i && issue_year.to_i <= 2020 },
+    "eyr" => -> (exp_year)   { 2020 <= exp_year.to_i && exp_year.to_i <= 2030 },
+    "hcl" => -> (hair_color) { /#(\d|[a-f]){6}/.match? hair_color }, 
+    "pid" => -> (passport_id) { /\A\d{9}\z/.match? passport_id },
+    "ecl" => -> (eye_color)  { %w{amb blu brn gry grn hzl oth}.include? eye_color },
+    "hgt" => -> (height) do 
+      case height
+      when /(\d+)cm/
+        150 <= $1.to_i && $1.to_i <= 193 
+      when /(\d+)in/
+        59 <= $1.to_i && $1.to_i <= 76
+      else
+        false
+      end
+    end,
+  }
+  REQUIRED_FIELDS = FIELD_RULES.keys
 
   def self.scanner(feed)
     feed.chunk_while {|line| line != ""}
@@ -40,10 +64,11 @@ class Passport
 
   def valid?
     return false unless all_required_fields_present?
-    true
+    FIELD_RULES.map { |field, rule| rule.call(@data[field]) }
+               .all? true
   end
   
   def all_required_fields_present?
-    (OUR_HACK & @data.keys).size == OUR_HACK.size
+    (REQUIRED_FIELDS & @data.keys).size == REQUIRED_FIELDS.size
   end
 end
--- 2020/ruby/spec/day04_spec.rb
+++ 2020/ruby/spec/day04_spec.rb
@@ -23,7 +23,83 @@ describe Passport do
 
   describe "Part 1" do
     it "validates passports but with our hack" do
-      expect(scanned_input.map(&:valid?)).to eq [true, false, true, false]
+      expect(scanned_input.map(&:all_required_fields_present?)).to eq [true, false, true, false]
+    end
+  end
+
+  describe "Part 2" do
+    it "example invalid passports" do
+      input = <<~INVALID
+                eyr:1972 cid:100
+                hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+                iyr:2019
+                hcl:#602927 eyr:1967 hgt:170cm
+                ecl:grn pid:012533040 byr:1946
+
+                hcl:dab227 iyr:2012
+                ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+                hgt:59cm ecl:zzz
+                eyr:2038 hcl:74454a iyr:2023
+                pid:3556412378 byr:2007
+              INVALID
+      invalid_passports = Passport.scanner(input.split("\n"))
+      expect(invalid_passports.map(&:valid?)).to eq [false, false, false, false]
+    end
+
+    it "example valid passports" do
+      input = <<~VALID
+                pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+                hcl:#623a2f
+                
+                eyr:2029 ecl:blu cid:129 byr:1989
+                iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+                
+                hcl:#888785
+                hgt:164cm byr:2001 iyr:2015 cid:88
+                pid:545766238 ecl:hzl
+                eyr:2022
+                
+                iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+              VALID
+      valid_passports = Passport.scanner(input.split("\n"))
+      expect(valid_passports.map(&:valid?)).to eq [true, true, true, true]
+    end
+
+    describe "field validation rules" do
+      it "validates birth year" do
+        birth_year_rule = Passport::FIELD_RULES["byr"]
+        expect(birth_year_rule.call("2002")).to be true
+        expect(birth_year_rule.call("2003")).to be false
+      end
+    
+      it "validates height" do
+        height_rule = Passport::FIELD_RULES["hgt"]
+        expect(height_rule.call("60in")).to be true
+        expect(height_rule.call("190cm")).to be true
+        expect(height_rule.call("190in")).to be false
+        expect(height_rule.call("190")).to be false
+      end
+
+      it "validates hair color" do
+        hair_color_rule = Passport::FIELD_RULES["hcl"]
+        expect(hair_color_rule.call("#123abc")).to be true
+        expect(hair_color_rule.call("#123abz")).to be false
+        expect(hair_color_rule.call("123abc")).to be false
+      end
+    
+      it "validates eye color" do
+        eye_color_rule = Passport::FIELD_RULES["ecl"]
+        expect(eye_color_rule.call("brn")).to be true
+        expect(eye_color_rule.call("wat")).to be false
+      end
+
+      it "validates passport id" do
+        passport_id_rule = Passport::FIELD_RULES["pid"]
+        expect(passport_id_rule.call("000000001")).to be true
+        expect(passport_id_rule.call("0123456789")).to be false
+      end
     end
   end
 end

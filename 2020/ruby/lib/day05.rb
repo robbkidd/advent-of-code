9122bc0e2bc6622229c42d6b057eee840e2b17cd
Robb Kidd <robb@thekidds.org>
2020-12-05 12:22:53 -0500

    now with binary shenanigans I didn't notice before

--- 2020/ruby/lib/day05.rb
+++ 2020/ruby/lib/day05.rb
@@ -1,22 +1,40 @@
 class Day05
   def self.go
-    puts "Part1: #{part1}"
-    puts "Part2: #{part2}"
+    day = new
+    puts "Part1: #{day.part1}"
+    puts "Part2: #{day.part2}"
+    puts ""
+    puts "ಠ_ಠ"
+    puts
+    puts "Or with _only_ binary shenanigans"
+    puts day.ಠ_ಠ
   end
 
-  def self.part1
+  def initialize
+    @input = File.read("../inputs/#{self.class.name.downcase}-input.txt")
+                 .split("\n")
+  end
+
+  def part1
     boarding_pass_seat_ids.max
   end
 
-  def self.part2
+  def part2
     seat_ids = boarding_pass_seat_ids.sort
     ((seat_ids[0]..seat_ids[-1]).to_a - seat_ids).first
   end
 
-  def self.boarding_pass_seat_ids
-    File.read('../inputs/day05-input.txt')
-        .split("\n")
-        .map { |code| BoardingPass.new(code).seat_id }
+  def boarding_pass_seat_ids
+    @input.map { |code| BoardingPass.new(code).seat_id }
+  end
+
+  def ಠ_ಠ
+    # OK, the whole thing can be done with just binary mathy math
+    # the row is already x8 because of its digits placement
+    seat_ids = @input.map { |code| code.tr("FL", "0").tr("BR", "1").to_i(2) }
+                     .sort
+    
+    [seat_ids[-1], ((seat_ids[0]..seat_ids[-1]).to_a - seat_ids).first]
   end
 end
 
--- 2020/ruby/spec/day05_spec.rb
+++ 2020/ruby/spec/day05_spec.rb
@@ -15,3 +15,22 @@ describe "Part 1 examples" do
     expect(pass.seat_id).to eq 820
   end
 end
+
+describe "ಠ_ಠ" do
+
+  def just_translate_it_to_binary(code)
+    code.tr("FL", "0").tr("BR", "1").to_i(2)
+  end
+
+  it "BFFFBBFRRR: row 70, column 7, seat ID 567." do
+    expect(just_translate_it_to_binary("BFFFBBFRRR")).to eq 567
+  end
+
+  it "FFFBBBFRRR: row 14, column 7, seat ID 119." do
+    expect(just_translate_it_to_binary("FFFBBBFRRR")).to eq 119
+  end
+
+  it "BBFFBBFRLL: row 102, column 4, seat ID 820." do
+    expect(just_translate_it_to_binary("BBFFBBFRLL")).to eq 820
+  end
+end

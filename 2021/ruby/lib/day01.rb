0fce248162d09122746f65c8573e42584c0020b0
Robb Kidd <robb@thekidds.org>
2021-12-01 08:59:08 -0500

    Dear VSCode, I never want tabs in my Ruby.

--- 2021/ruby/lib/day01.rb
+++ 2021/ruby/lib/day01.rb
@@ -1,36 +1,36 @@
 class Day01
-	def self.go
-		day = Day01.new
-		puts "Part 1: #{day.part1}"
-		puts "Part 2: #{day.part2}"
-	end
+  def self.go
+    day = Day01.new
+    puts "Part 1: #{day.part1}"
+    puts "Part 2: #{day.part2}"
+  end
 
-	def part1
-		sonar_sweep = sonar_sweep_report
-		num_increases(sonar_sweep)
-	end
+  def part1
+    sonar_sweep = sonar_sweep_report
+    num_increases(sonar_sweep)
+  end
 
-	def part2
-		sonar_sweep = sonar_sweep_report
-		sliding_window_increases(sonar_sweep)
-	end
+  def part2
+    sonar_sweep = sonar_sweep_report
+    sliding_window_increases(sonar_sweep)
+  end
 
-	def num_increases(sonar_sweep)
-		sonar_sweep
-			.each_cons(2)
-			.filter { |prv,nxt| prv < nxt }
-			.length
-	end
+  def num_increases(sonar_sweep)
+    sonar_sweep
+      .each_cons(2)
+      .filter { |prv,nxt| prv < nxt }
+      .length
+  end
 
-	def sliding_window_increases(sonar_sweep)
-		num_increases(
-			sonar_sweep
-				.each_cons(3)
-				.map {|window| window.reduce(&:+) }
-		)
-	end
+  def sliding_window_increases(sonar_sweep)
+    num_increases(
+      sonar_sweep
+        .each_cons(3)
+        .map {|window| window.reduce(&:+) }
+    )
+  end
 
-	def sonar_sweep_report
+  def sonar_sweep_report
     File.read('../inputs/day01-input.txt').split("\n").map(&:to_i)
   end
 end

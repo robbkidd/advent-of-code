d27bbfaba128e55cc214cfd30f22049c6e482d7a
Robb Kidd <robb@thekidds.og>
2021-12-05 21:15:55 -0500

    doc tests are cool

--- 2021/ruby/day05.rb
+++ 2021/ruby/day05.rb
@@ -27,6 +27,25 @@ class Day05
       .length
   end
 
+
+  EXAMPLE_DIAGRAM = <<~DIAGRAM
+    1.1....11.
+    .111...2..
+    ..2.1.111.
+    ...1.2.2..
+    .112313211
+    ...1.2....
+    ..1...1...
+    .1.....1..
+    1.......1.
+    222111....
+  DIAGRAM
+
+  # @example part 2 diagram
+  #   part2_example_diagram =
+  #   d = Day05.new(Day05::EXAMPLE_INPUT)
+  #   d.to_s #=> Day05::EXAMPLE_DIAGRAM
+  #
   def to_s
     intersections = parse
       .map{ |line| to_vent_line(line, skip_diagonals: false) }
@@ -51,6 +70,19 @@ class Day05
     File.read('../inputs/day05-input.txt')
   end
 
+  # @example vertical line
+  #   d = ::Day05.new
+  #   d.to_vent_line([[1,1],[1,3]]) #=> [[1, 1], [1, 2], [1, 3]]
+  # @example horizontal line
+  #   d = Day05.new
+  #   d.to_vent_line([[9,7],[7,7]]) #=> [[9, 7], [8, 7], [7, 7]]
+  # @example diagonal lines
+  #   d = Day05.new(Day05::EXAMPLE_INPUT)
+  #   d.to_vent_line([[1,1],[3,3]]) #=> []
+  #   d.to_vent_line([[1,1],[3,3]], skip_diagonals: true) #=> []
+  #   d.to_vent_line([[1,1],[3,3]], skip_diagonals: false) #=> [[1,1], [2,2], [3,3]]
+  #   d.to_vent_line([[9,7],[7,9]], skip_diagonals: false) #=> [[9,7], [8,8], [7,9]]
+  #
   def to_vent_line(end_coords, skip_diagonals: true)
     start, finish = end_coords
     case
@@ -77,6 +109,10 @@ class Day05
     e1 > e2 ? e1.downto(e2).to_a : e1.upto(e2).to_a
   end
 
+  # @example parsing!
+  #   d = Day05.new(Day05::EXAMPLE_INPUT)
+  #   d.parse.first #=> [[0,9],[5,9]]
+  #
   def parse
     @data
       .split("\n")
@@ -98,54 +134,4 @@ class Day05
     0,0 -> 8,8
     5,5 -> 8,2
   INPUT
-end
-
-require 'minitest'
-
-class TestDay05 < Minitest::Test
-  def test_parse_input
-    d = Day05.new(Day05::EXAMPLE_INPUT)
-    assert_equal [[0,9],[5,9]], d.parse.first
-  end
-
-  def test_to_vent_line
-    d = Day05.new
-    assert_equal [[1, 1], [1, 2], [1, 3]], d.to_vent_line([[1,1],[1,3]])
-  end
-
-  def test_other_line
-    d = Day05.new
-    assert_equal [[9, 7], [8, 7], [7, 7]], d.to_vent_line([[9,7],[7,7]])
-  end
-
-  def test_a_diagonal
-    d = Day05.new(Day05::EXAMPLE_INPUT)
-    assert_equal [], d.to_vent_line([[1,1],[3,3]])
-    assert_equal [], d.to_vent_line([[1,1],[3,3]], skip_diagonals: true)
-    assert_equal [[1,1], [2,2], [3,3]], d.to_vent_line([[1,1],[3,3]], skip_diagonals: false)
-    assert_equal [[9,7], [8,8], [7,9]], d.to_vent_line([[9,7],[7,9]], skip_diagonals: false)
-  end
-
-  def test_printing_the_grid
-    part2_example_diagram = <<~DIAGRAM
-      1.1....11.
-      .111...2..
-      ..2.1.111.
-      ...1.2.2..
-      .112313211
-      ...1.2....
-      ..1...1...
-      .1.....1..
-      1.......1.
-      222111....
-    DIAGRAM
-    d = Day05.new(Day05::EXAMPLE_INPUT)
-    assert_equal part2_example_diagram, d.to_s
-  end
-end
-
-if ENV.key? 'TEST'
-  require 'minitest/autorun'
-else
-  Day05.go
 end

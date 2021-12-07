cf75c1c37df5692cce7506409baa499cb16ab3c3
Robb Kidd <robb@thekidds.og>
2021-12-07 09:47:59 -0500

    part 2
    Brute! Force!
    
    Takes ~30 seconds, so I suspect there is some mathy math for this.

--- 2021/ruby/day07.rb
+++ 2021/ruby/day07.rb
@@ -8,35 +8,54 @@ class Day07
   def initialize(input=nil)
     @input = (input || real_input).chomp.split(",").map(&:to_i)
     @min, @max = @input.minmax
+    @avg = (@input.reduce(&:+) / @input.length).round
   end
 
   # @example
   #   d = Day07.new(EXAMPLE_INPUT)
-  #   d.part1 #=> 37
+  #   d.part1 #=> [2, 37]
   def part1
-    avg = (@input.reduce(&:+) / @input.length).round
-
-    candidates = avg
-      .upto(@max)
-      .to_a
-      .zip(avg.downto(@min).to_a)
-      .reject { |pair| pair.any? {|p| p.nil?} }
-      .flatten
-      .uniq
-
-    candidates
+    likely_candidates
       .map {|c|
-        [ c ,fuel_cost_to_get_all_crabs_to(c) ]
+        [ c , naive_fuel_cost_to_get_all_crabs_to(c) ]
       }
-      .min_by {|position, fuel_cost| fuel_cost}[1]
+      .min_by {|position, fuel_cost| fuel_cost}
   end
 
+  # @example
+  #   d = Day07.new(EXAMPLE_INPUT)
+  #   d.part2 #=> [5, 168]
   def part2
+    likely_candidates
+      .map {|c|
+        [ c , crab_engineering_fuel_cost_to_get_all_crabs_to(c) ]
+      }
+      .min_by {|position, fuel_cost| fuel_cost}
   end
 
-  def fuel_cost_to_get_all_crabs_to(position)
+  def likely_candidates
+    @candidates ||=
+      @avg
+        .upto(@max)
+        .to_a
+        .zip(@avg.downto(@min).to_a)
+        .reject { |pair| pair.any? {|p| p.nil?} }
+        .flatten
+        .uniq
+  end
+
+  def naive_fuel_cost_to_get_all_crabs_to(position)
     @input
-      .map { |i| (i - position).abs }
+      .map { |i| (i - position).abs } # distance
+      .reduce(&:+)
+  end
+
+  def crab_engineering_fuel_cost_to_get_all_crabs_to(position)
+    @input
+      .map { |i|
+        d = (i - position).abs # distance
+        d != 0 ? 1.upto(d).reduce(&:+) : d # fuel_cost
+      }
       .reduce(&:+)
   end
 

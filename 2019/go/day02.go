8789e76f2bbffb1cf58bbfbf1449b422f13ab279
Robb Kidd <robb@thekidds.og>
2021-12-07 21:53:30 -0500

    part 2

--- 2019/go/day02.go
+++ 2019/go/day02.go
@@ -10,12 +10,14 @@ import (
 
 func main() {
 	answerP1, _ := p1()
-	fmt.Printf("Part 1: %v\n", answerP1)
+	fmt.Printf("Part 1: %d\n", answerP1)
+	fmt.Printf("Part 2: %d\n", p2())
 }
 
 func p1() (int, error) {
+	gravityAssistProgram := input()
 	cpu := new(intcode.IntcodeComputer)
-	cpu.Load(input())
+	cpu.Load(gravityAssistProgram)
 	// restore the gravity assist program
 	// to the "1202 program alarm" state
 	cpu.Hack(1, 12)
@@ -31,6 +33,30 @@ func p1() (int, error) {
 		return -0, fmt.Errorf("🖥😢 %v", err)
 	}
 	return output, nil
+}
+
+func p2() int {
+	noun, verb := findInputsForMoonLandingDate()
+	return 100*noun + verb
+}
+
+func findInputsForMoonLandingDate() (int, int) {
+	moonLandingDate := 19690720
+	gravityAssistProgram := input()
+	cpu := new(intcode.IntcodeComputer)
+	for noun := 0; noun <= 99; noun++ {
+		for verb := 0; verb <= 99; verb++ {
+			cpu.Load(gravityAssistProgram)
+			cpu.Hack(1, noun)
+			cpu.Hack(2, verb)
+			cpu.Run()
+			output, _ := cpu.Read(0)
+			if output == moonLandingDate {
+				return noun, verb
+			}
+		}
+	}
+	return -1, -1
 }
 
 func input() string {

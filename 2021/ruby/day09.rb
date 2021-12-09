061ef4f4ed8d4c1ba8f1d0e70c319efbb03de48a
Robb Kidd <robb@thekidds.org>
2021-12-09 17:38:54 -0500

    part2

--- 2021/ruby/day09.rb
+++ 2021/ruby/day09.rb
@@ -1,3 +1,4 @@
+require 'set'
 class Day09
   def self.go
     day = new
@@ -5,20 +6,68 @@ class Day09
     puts "Part 2: #{day.part2}"
   end
 
-  attr_reader :input, :depths
+  attr_reader :input, :depths, :row_bounds, :column_bounds
 
   def initialize(input=nil)
     @input = input || real_input
     @depths = Hash.new(:out_of_bounds)
+    parse_input
   end
 
   # @example
   #   d = Day09.new(EXAMPLE_INPUT)
   #   d.part1 #=> 15
   def part1
-    parse_input
+    lowest_points
+      .values
+      .map{ |low_point| low_point + 1 }
+      .reduce(&:+)
+  end
 
-    depths
+  # @example
+  #   d = Day09.new(EXAMPLE_INPUT)
+  #   d.part2 #=> 1134
+  def part2
+    basins
+      .map{ |b| b.length }
+      .sort[-3..-1]
+      .reduce(&:*)
+  end
+
+  def basins
+    @basins ||= lowest_points
+      .map {|location, _depth|
+        survey_basin(location)
+      }
+  end
+
+  # @example
+  #   d = Day09.new(EXAMPLE_INPUT)
+  #   d.survey_basin([0,1]) #=> Set.new([[0,0],[0,1],[1,0]])
+  def survey_basin(location)
+    basin = Set.new.add(location)
+    survey = [location]
+    while !survey.empty?
+      check_loc = survey.pop
+      if depths[check_loc] != 9
+        adjacent_locations(check_loc)
+          .reject { |neighbor| depths[neighbor] == :out_of_bounds }
+          .each do |neighbor|
+            if !basin.include?(neighbor)
+              basin << neighbor if depths[neighbor] != 9
+              survey.unshift(neighbor)
+            end
+          end
+      end
+    end
+    basin
+  end
+
+  # @example
+  #   d = Day09.new(EXAMPLE_INPUT)
+  #   d.lowest_points #=> {[0, 1]=>1, [0, 9]=>0, [2, 2]=>5, [4, 6]=>5}
+  def lowest_points
+    @lowest_points ||= depths
       .select{ |location, measured_depth|
         adjacent_locations(location)
           .map{ |neighbor_loc|
@@ -29,9 +78,6 @@ class Day09
           }
           .empty?
       }
-      .values
-      .map{ |low_point| low_point + 1 }
-      .reduce(&:+)
   end
 
   ADJACENCY_DIRECTIONS = {
@@ -55,10 +101,14 @@ class Day09
     }
   end
 
-  def part2
-  end
-
   # Updates @depths with values from input
+  # @example
+  #   d = Day09.new(EXAMPLE_INPUT)
+  #   d.parse_input
+  #   d.row_bounds #=> (0..4)
+  #   d.row_bounds.cover?(4) #=> true
+  #   d.column_bounds #=> (0..9)
+  #   d.column_bounds.cover?(10) #=> false
   def parse_input
     input
       .split("\n")
@@ -68,6 +118,44 @@ class Day09
           depths[[r,c]] = measured_depth
         }
       }
+
+    @row_bounds,
+    @column_bounds = depths
+      .keys
+      .transpose
+      .map{ |dimension| Range.new(*dimension.minmax) }
+  end
+
+  # Print lowest points or basins?
+  #
+  # @example
+  #   d = Day09.new(EXAMPLE_INPUT)
+  #   d.display_heightmap
+  #   d.display_heightmap(:basins)
+  def display_heightmap(type = :lowest_points)
+    puts
+    @row_bounds.each do |row|
+      @column_bounds.each do |column|
+        case type
+        when :lowest_points
+          if lowest_points.keys.include?([row,column])
+            print "\e[7m#{depths[[row,column]]}\e[0m"
+          else
+            print depths[[row,column]]
+          end
+        when :basins
+          if basins.map{|b| b.include? [row,column]}.any?
+            print "\e[7m#{depths[[row,column]]}\e[0m"
+          else
+            print depths[[row,column]]
+          end
+        else
+          puts "I don't know how to print #{type}."
+        end
+      end
+      puts
+    end
+    puts
   end
 
   def real_input

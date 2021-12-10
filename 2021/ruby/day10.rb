a7fe5397e070740f3de588f3bebb44b01b660b23
Robb Kidd <robb@thekidds.org>
2021-12-10 12:31:46 -0500

    part 2

--- 2021/ruby/day10.rb
+++ 2021/ruby/day10.rb
@@ -17,22 +17,37 @@ class Day10
     input
       .split("\n")
       .map { |line| analyze_line(line) }
-      .filter { |result, _char| :corrupted == result }
+      .filter { |result, _| :corrupted == result }
       .map { |_result, char| CLOSERS[char][:corruption_points] }
       .reduce(&:+)
   end
 
+  # @example
+  #   day.part2 #=> 288957
   def part2
+    completion_scores = input
+      .split("\n")
+      .map { |line| analyze_line(line) }
+      .filter { |result, _| :incomplete == result }
+      .map { |_result, opener_stack|
+        opener_stack
+          .reverse
+          .map{ |opener| OPENERS[opener] }
+      }
+      .map {|completions| completion_score(completions) }
+      .sort
+
+    completion_scores[(completion_scores.length / 2).floor]
   end
 
   # ~Coffee~ Points are for closers.
   CLOSERS = {
-    ')' => {opener: '(', corruption_points: 3},
-    ']' => {opener: '[', corruption_points: 57},
-    '}' => {opener: '{', corruption_points: 1197},
-    '>' => {opener: '<', corruption_points: 25137},
+    ')' => {opener: '(', corruption_points: 3, completion_points: 1},
+    ']' => {opener: '[', corruption_points: 57, completion_points: 2},
+    '}' => {opener: '{', corruption_points: 1197, completion_points: 3},
+    '>' => {opener: '<', corruption_points: 25137, completion_points: 4},
   }
-  OPENERS = CLOSERS.values.map{|c| c[:opener]}
+  OPENERS = CLOSERS.map{ |closer, h| [h[:opener], closer] }.to_h
 
   # @example
   #   day.analyze_line('{([(<{}[<>[]}>{[]{[(<()>') #=> [:corrupted, '}']
@@ -45,7 +60,7 @@ class Day10
     line
       .chars
       .each do |char|
-        if OPENERS.include?(char)
+        if OPENERS.keys.include?(char)
           stack.push(char)
           next
         else
@@ -56,6 +71,18 @@ class Day10
         end
       end
     return stack.empty? ? [:ok, nil] : [:incomplete, stack]
+  end
+
+  # @example
+  #   day.completion_score('}}]])})]'.chars) #=> 288957
+  #   day.completion_score(')}>]})'.chars) #=> 5566
+  #   day.completion_score('}}>}>))))'.chars) #=> 1480781
+  #   day.completion_score(']]}}]}]}>'.chars) #=> 995444
+  #   day.completion_score('])}>'.chars) #=> 294
+  def completion_score(closers)
+    closers.reduce(0) do |total_score, closer|
+      (total_score * 5) + CLOSERS.fetch(closer).fetch(:completion_points)
+    end
   end
 
   def real_input

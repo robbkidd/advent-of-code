d3964edd6457400c24792d8c030fe422509cef98
Robb Kidd <robb@thekidds.og>
2021-12-20 20:07:26 -0500

    welp, part 1 passes for example, but not real input

--- 2021/ruby/day20.rb
+++ 2021/ruby/day20.rb
@@ -13,7 +13,16 @@ class Day20
     parse_input
   end
 
+  # @example
+  #   day.part1 #=> 35
   def part1
+    output_image = @input_image
+    puts ugly_christmas_sweater(display_image(output_image))
+    2.times do
+      output_image = enhance(output_image)
+      puts ugly_christmas_sweater(display_image(output_image))
+    end
+    output_image.keys.count
   end
 
   def part2
@@ -23,20 +32,79 @@ class Day20
   DARK_PIXEL = ".".freeze
 
   # @example
-  #   day.display_image(day.input_image) #=> Day20::EXAMPLE_INPUT.split("\n\n")[1]
-  def display_image(image)
-    row_bounds,
-    column_bounds = image
-      .keys
-      .transpose
-      .map{ |dimension| Range.new(*dimension.minmax) }
+  #   one_round = day.enhance(day.input_image)
+  #   day.display_image(one_round) #=> EXAMPLE_ONE_ENHANCEMENT
+  def enhance(image)
+    row_bounds, column_bounds = image_bounds(image)
 
-    row_bounds.map { |r|
-      column_bounds.map { |c|
-        image[[c,r]]
+    enhanced_image = Hash.new(DARK_PIXEL)
+
+    column_bounds.map { |c|
+      row_bounds.map { |r|
+        new_pixel = convert_pixel([c,r], image)
+        enhanced_image[[c,r]] = new_pixel if new_pixel == LIGHT_PIXEL
+      }
+    }
+
+    enhanced_image
+  end
+
+  # @example
+  #   day.convert_pixel([2,2], day.input_image) #=> "#"
+  def convert_pixel(pixel, image)
+    @image_enhancement_algorithm.at(
+      pixel_grid(pixel)
+        .map{|loc| image[loc] }
+        .join
+        .tr(".#", "01")
+        .to_i(2)
+    )
+  end
+
+  # @example
+  #   PIXEL_OFFSETS.include?([0,0]) #=> true
+  PIXEL_OFFSETS = [
+    [-1,-1], [ 0,-1], [ 1,-1],
+    [-1, 0], [ 0, 0], [ 1, 0],
+    [-1, 1], [ 0, 1], [ 1, 1]
+  ].freeze
+
+  # @example
+  #   day.pixel_grid([5,10]) #=> [[4, 9], [5, 9], [6, 9], [4, 10], [5, 10], [6, 10], [4, 11], [5, 11], [6, 11]]
+  def pixel_grid(pixel)
+    PIXEL_OFFSETS.map{ |offset|
+      pixel
+        .zip(offset)
+        .map { |p| p.reduce(&:+) }
+    }
+  end
+
+  def display_image(image)
+    x_bounds, y_bounds = image_bounds(image)
+
+    y_bounds.map { |y|
+      x_bounds.map { |x|
+        image[[x,y]]
       }.join
     }.join("\n")
     .concat("\n")
+  end
+
+  def ugly_christmas_sweater(output)
+    output
+      .gsub(/#/, "\e[41m\e[1m#\e[0m")
+      .gsub(/\./, "\e[32m.\e[0m")
+  end
+
+  # @example
+  #   two_dots_img = {[-10,-20] => "#", [100,200] => "#"}
+  #   day.image_bounds(two_dots_img) #=> [(-14..104), (-24..204)]
+  def image_bounds(image)
+    image
+      .keys
+      .transpose
+      .map{ |dimension| dimension.minmax }
+      .map{ |min, max| Range.new(min-4, max+4) }
   end
 
   # @example
@@ -51,9 +119,9 @@ class Day20
     image
       .split("\n")
       .map{|row| row.chars}
-      .each_with_index{ |row, r|
-        row.each_with_index{ |pixel, c|
-          @input_image[[c,r]] = pixel if LIGHT_PIXEL == pixel
+      .each_with_index{ |row, y|
+        row.each_with_index{ |pixel, x|
+          @input_image[[x,y]] = pixel if LIGHT_PIXEL == pixel
         }
       }
   end
@@ -71,4 +139,40 @@ class Day20
     ..#..
     ..###
   INPUT
+
+  EXAMPLE_BEFORE_ENHANCEMENT = <<~ORIG
+    ...............
+    ...............
+    ...............
+    ...............
+    ...............
+    .....#..#......
+    .....#.........
+    .....##..#.....
+    .......#.......
+    .......###.....
+    ...............
+    ...............
+    ...............
+    ...............
+    ...............
+  ORIG
+
+  EXAMPLE_ONE_ENHANCEMENT = <<~ENHANCE
+    ...............
+    ...............
+    ...............
+    ...............
+    .....##.##.....
+    ....#..#.#.....
+    ....##.#..#....
+    ....####..#....
+    .....#..##.....
+    ......##..#....
+    .......#.#.....
+    ...............
+    ...............
+    ...............
+    ...............
+  ENHANCE
 end

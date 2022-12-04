fede8e91b9b622cce25828f09d8a7bfad53a0ad3
Robb Kidd <robb@thekidds.og>
2022-12-04 13:11:45 -0500

    refactor to types and a function

--- 2022/go/day04/main.go
+++ 2022/go/day04/main.go
@@ -17,25 +17,46 @@ func main() {
 func p1(lines []string) int {
 	var fullyCovered int
 	for _, line := range lines {
-		pair := strings.Split(line, ",")
-		left := strings.Split(pair[0], "-")
-		right := strings.Split(pair[1], "-")
-		leftStart, _ := strconv.Atoi(left[0])
-		leftStop, _ := strconv.Atoi(left[1])
-		rightStart, _ := strconv.Atoi(right[0])
-		rightStop, _ := strconv.Atoi(right[1])
+		pair := lineToPair(line)
 
-		if leftStart >= rightStart && leftStop <= rightStop {
-			fullyCovered++
-			continue
-		}
-
-		if leftStart <= rightStart && leftStop >= rightStop {
+		if fullyCovers(pair) {
 			fullyCovered++
 			continue
 		}
 	}
 	return fullyCovered
+}
+
+func fullyCovers(pair Pair) bool {
+	if pair.left.start >= pair.right.start && pair.left.stop <= pair.right.stop {
+		return true
+	}
+	if pair.left.start <= pair.right.start && pair.left.stop >= pair.right.stop {
+		return true
+	}
+	return false
+}
+
+func lineToPair(line string) Pair {
+	p := strings.Split(line, ",")
+	left := strings.Split(p[0], "-")
+	right := strings.Split(p[1], "-")
+
+	leftStart, _ := strconv.Atoi(left[0])
+	leftStop, _ := strconv.Atoi(left[1])
+	rightStart, _ := strconv.Atoi(right[0])
+	rightStop, _ := strconv.Atoi(right[1])
+
+	return Pair{
+		left: Assignment{
+			start: leftStart,
+			stop:  leftStop,
+		},
+		right: Assignment{
+			start: rightStart,
+			stop:  rightStop,
+		},
+	}
 }
 
 func p2(input []string) int {

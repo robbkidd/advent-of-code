98cd485e4a61d3fa56f1b44665d5a66310779934
Robb Kidd <robb@thekidds.og>
2023-12-02 12:58:47 -0500

    Day 2 again, but classier

--- 2023/ruby/day02.rb
+++ 2023/ruby/day02.rb
@@ -71,6 +71,118 @@ class Day02 < Day # >
     [id, sets]
   end
 
+  # === Again, But Classier ===
+
+  # @example
+  #   day.part1_with_class #=> 8
+  def part1_with_class
+    input
+      .split("\n")
+      .map { |line| Game.new.parse(line) }
+      .reject { |game|                       # reject any game wherein ...
+        game.sets.any? { |set|               # any set has too many of a color
+          set[:red] > 12 ||
+            set[:green] > 13 ||
+            set[:blue] > 14
+        }
+      }
+      .map { |game| game.id }
+      .reduce(:+)
+  end
+
+  # @example
+  #   day.part2_with_class #=> 2286
+  def part2_with_class
+    input
+      .split("\n")
+      .map { |line| Game.new.parse(line) }
+      .map { |game| game.minimum_set }
+      .map { |set| set.power}
+      .reduce(:+)
+  end
+
+  class Game
+    attr_reader :id, :sets
+
+    # @example
+    #   game = Day02::Game.new
+    #   game.parse('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green')
+    #   game.id #=> 1
+    #   game.sets.first.blue #=> 3
+    #   game.sets.last.green #=> 2
+    def parse(line)
+      id, sets = line.split(': ')
+      @id = id.slice(/\d+$/).to_i
+
+      @sets = sets
+        .split('; ')
+        .map { |set|
+          Subset.new.parse(set)
+        }
+
+      self
+    end
+
+    # @example
+    #   game = Day02::Game.new
+    #   game.parse('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green')
+    #   min_set = game.minimum_set
+    #   min_set.blue #=> 6
+    #   min_set.green #=> 2
+    #   min_set.red #=> 4
+    def minimum_set
+      sets
+        .reduce { |memo, set|
+          [:red, :green, :blue].each do |color|
+            memo[color] = [memo[color], set[color]].max
+          end
+          memo
+        }
+    end
+  end
+
+  class Subset
+    attr_accessor :blue, :green, :red
+
+    def initialize
+      @blue = 0
+      @green = 0
+      @red = 0
+    end
+
+    # @example
+    #   subset = Day02::Subset.new
+    #   subset.parse('4 red, 2 green, 6 blue')
+    #   subset.power #=> 48
+    def power
+      blue * green * red
+    end
+
+    def [](color)
+      self.send("#{color}".to_sym)
+    end
+
+    def []=(color, count)
+      self.send("#{color}=".to_sym, count)
+    end
+
+    # @example
+    #   subset = Day02::Subset.new
+    #   subset.parse('3 blue, 4 red')
+    #   subset.blue   #=> 3
+    #   subset.red    #=> 4
+    #   subset.green  #=> 0
+    def parse(input)
+      input
+        .split(", ")
+        .each { |cubes|
+          count, color = cubes.split(" ")
+          self.send("#{color}=".to_sym, count.to_i)
+        }
+      self
+    end
+  end
+
   EXAMPLE_INPUT = <<~INPUT
     Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue

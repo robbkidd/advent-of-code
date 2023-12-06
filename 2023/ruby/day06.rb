cae5ff6c8e1bce817452e51dc83e868de82d3b78
Robb Kidd <robb@thekidds.og>
2023-12-06 07:53:28 -0500

    day 6, part 2
    brute, but good enough

--- 2023/ruby/day06.rb
+++ 2023/ruby/day06.rb
@@ -1,4 +1,5 @@
 require_relative 'day'
+require 'parallel'
 
 class Day06 < Day # >
 
@@ -7,19 +8,26 @@ class Day06 < Day # >
   def part1
     input
       .each_line
-      .map { |line| line.split(/\s+/)[1..].map(&:to_i) }
+      .map { |line| line.scan(/\d+/).map(&:to_i) }
       .transpose
       .map {|time, record_distance|
         1.upto(time)
-          .select { |hold_time| hold_time * (time-hold_time) > record_distance }
+          .select { |hold| hold * (time-hold) > record_distance }
           .count
       }
       .reduce(&:*)
   end
 
   # @example
-  #   day.part2 #=> 'how are you'
+  #   day.part2 #=> 71503
   def part2
+    time, record_distance = input
+                              .each_line
+                              .map { |line| line.scan(/\d+/).join('').to_i }
+
+    1.upto(time)
+      .select { |hold| hold * (time-hold) > record_distance }
+      .count
   end
 
   EXAMPLE_INPUT = <<~INPUT

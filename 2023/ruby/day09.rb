95b73263fe66fb225a021459aed8d5b849a891ab
Robb Kidd <robb@thekidds.og>
2023-12-09 20:12:36 -0500

    instead of two copies of the functions, same func with reversed input!

--- 2023/ruby/day09.rb
+++ 2023/ruby/day09.rb
@@ -3,25 +3,10 @@ require_relative 'day'
 class Day09 < Day # >
 
   # @example
-  #  day.oasis_report #=> [[0, 3, 6, 9, 12, 15], [1, 3, 6, 10, 15, 21], [10, 13, 16, 21, 30, 45]]
-  def oasis_report
-    @oasis_report ||=
-      input
-        .each_line
-        .map { |line| line.split(" ").map(&:to_i) }
-  end
-
-  # @example
   #   day.part1 #=> 114
   def part1
     oasis_report
-      .map { |history|
-        sequences = [history]
-        while !sequences.last.all?(0) do
-          sequences << sequences.last.each_cons(2).map { |a,b| b - a }
-        end                                 #                  ^^^^^
-        sequences.map(&:last).reduce(&:+)   # <--- last and next-minus-prev
-      }
+      .map { |value_history| next_in_sequence(value_history)}
       .reduce(&:+)
   end
 
@@ -29,14 +14,25 @@ class Day09 < Day # >
   #   day.part2 #=> 2
   def part2
     oasis_report
-      .map { |history|
-        sequences = [history]
-        while !sequences.last.all?(0) do
-          sequences << sequences.last.each_cons(2).map { |a,b| a - b }
-        end                                 #                  ^^^^^
-        sequences.map(&:first).reduce(&:+)  # <--- first and prev-minus-next
-      }
+      .map { |value_history| next_in_sequence(value_history.reverse)}
       .reduce(&:+)
+  end
+
+  def next_in_sequence(sequence)
+    sequences = [sequence]
+    until sequences.last.all?(0) do
+      sequences << sequences.last.each_cons(2).map { |a,b| b - a }
+    end
+    sequences.map(&:last).reduce(&:+)
+  end
+
+  # @example
+  #  day.oasis_report #=> [[0, 3, 6, 9, 12, 15], [1, 3, 6, 10, 15, 21], [10, 13, 16, 21, 30, 45]]
+  def oasis_report
+    @oasis_report ||=
+      input
+        .each_line
+        .map { |line| line.split(" ").map(&:to_i) }
   end
 
   EXAMPLE_INPUT = <<~INPUT

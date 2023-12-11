ce010db03a84296f3614df90cd1d4f61f1aaf813
Robb Kidd <robb@thekidds.og>
2023-12-11 09:09:22 -0500

    refactor into a class to do the maths
    Of course my part 1 was too naive for part 2. Refactoring in prep for computing differently.

--- 2023/ruby/day11.rb
+++ 2023/ruby/day11.rb
@@ -6,83 +6,18 @@ class Day11 < Day # >
   # @example
   #   day.part1 #=> 374
   def part1
-    expanded_universe =
-      Grid.new(
-        expand_cosmically(
-          scan(input)
-        )
-      )
+    universe = CosmicExpansion.new(input)
 
-    galaxies = []
-    expanded_universe.parse do |coords, value|
-      galaxies << coords if value == GALAXY
-    end
-
-    galaxies
+    universe
+      .galaxies
       .combination(2)
-      .map { |here, there| expanded_universe.manhattan_distance(here, there) }
+      .map { |here, there| universe.distance_between(here, there) }
       .reduce(&:+)
   end
 
   # @example
   # day.part2 #=> 'how are you'
   def part2
-  end
-
-
-  # @example
-  #   simple_input = "...\n.#.\n...\n"
-  #   day.scan(simple_input) #=> [[".",".","."],[".","#","."],[".",".","."]]
-  def scan(image)
-    image
-      .split("\n")
-      .map(&:chars)
-  end
-
-  EMPTY_SPACE = '.'
-  GALAXY = '#'
-
-  # @example
-  #  scanned_image = day.scan(EXAMPLE_INPUT)
-  #  input_expanded = day.expand_cosmically(scanned_image)
-  #  input_expanded #=> day.scan(EXPANDED_INPUT)
-  def expand_cosmically(scanned_image)
-    # dupe empty-space rows
-    expanded_rows = []
-    scanned_image
-      .each do |row|
-        n = row.all?(EMPTY_SPACE) ? 2 : 1
-        n.times { expanded_rows << row }
-      end
-
-    # pivot the table, dupe empty-space columns
-    expanded_columns = []
-    expanded_rows
-      .transpose
-      .each do |column|
-        n = column.all?(EMPTY_SPACE) ? 2 : 1
-        n.times { expanded_columns << column }
-      end
-
-    # pivot things back around to original orientation
-    # unnecessary for computing distances, but wanted for
-    # visualizing output
-    3.times { expanded_columns = expanded_columns.transpose }
-    expanded_columns
-  end
-
-  # @example simple input
-  #   simple_input = "...\n.#.\n...\n"
-  #   scanned_image = day.scan(simple_input)
-  #   scanned_image #=> [[".",".","."],[".","#","."],[".",".","."]]
-  #   day.display(scanned_image) #=> simple_input
-  # @example example input
-  #   scanned_image = day.scan(EXAMPLE_INPUT)
-  #   day.display(scanned_image) #=> EXAMPLE_INPUT
-  def display(scanned_image)
-    scanned_image
-      .map { |row| row.join('') }
-      .join("\n") + "\n"
   end
 
   EXAMPLE_INPUT = <<~INPUT
@@ -112,4 +47,67 @@ class Day11 < Day # >
     .........#...
     #....#.......
   INPUT
+end
+
+class CosmicExpansion
+  attr_reader :galaxies
+
+  def initialize(input)
+    @input = input
+    @galaxies = []
+    @grid =
+      Grid.new(
+        expand_cosmically(
+          scan(input)
+        )
+      )
+    @grid.parse do |coords, value|
+      galaxies << coords if value == GALAXY
+    end
+  end
+
+  EMPTY_SPACE = '.'
+  GALAXY = '#'
+
+  def distance_between(here, there)
+    @grid.manhattan_distance(here, there)
+  end
+
+  # @example
+  #   simple_input = "...\n.#.\n...\n"
+  #   universe = CosmicExpansion.new(simple_input)
+  #   universe.scan(simple_input) #=> [[".",".","."],[".","#","."],[".",".","."]]
+  def scan(image)
+    image
+      .split("\n")
+      .map(&:chars)
+  end
+
+  # @example
+  #  scanned_image = day.scan(Day11::EXAMPLE_INPUT)
+  #  input_expanded = day.expand_cosmically(scanned_image)
+  #  input_expanded #=> day.scan(Day11::EXPANDED_INPUT)
+  def expand_cosmically(scanned_image)
+    # dupe empty-space rows
+    expanded_rows = []
+    scanned_image
+      .each do |row|
+        n = row.all?(EMPTY_SPACE) ? 2 : 1
+        n.times { expanded_rows << row }
+      end
+
+    # pivot the table, dupe empty-space columns
+    expanded_columns = []
+    expanded_rows
+      .transpose
+      .each do |column|
+        n = column.all?(EMPTY_SPACE) ? 2 : 1
+        n.times { expanded_columns << column }
+      end
+
+    # pivot things back around to original orientation
+    # gotta get rid of this entirely for part 2, though
+    3.times { expanded_columns = expanded_columns.transpose }
+    expanded_columns
+  end
 end

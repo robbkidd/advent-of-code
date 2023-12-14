b59e82d0cf055b48f36a04ba5bacebccbb1ca07f
Robb Kidd <robb@thekidds.og>
2023-12-14 18:39:09 -0500

    Day 14, part 1 - but managing state in a Hash
    Also, Vectors. Trying out vector math instead of all this array
    zip-map-reducing to apply grid movements. Really should have
    considered that earlier.

--- 2023/ruby/day14.rb
+++ 2023/ruby/day14.rb
@@ -5,33 +5,14 @@ class Day14 < Day # >
   # @example
   #   day.part1 #=> 136
   def part1
-    columns =
-      input
-        .split("\n")
-        .map(&:chars)
-        .transpose      # pivot so that each array is a column
-        .reject {|column| !column.include?("O") } # don't bother with columns that don't have rounded rocks
-        .map(&:join)    # make it a string again
-        .map { |column|
-          column
-            .reverse              # north is now towards the end of the string
-            .split("#", -1)       # find the stretches between cube rocks, keep trailing empties
-            .map { |substring|
-              substring.chars.sort.join # roll rocks within stretches
-            }
-            .join("#")                  # put the cubes back in
-
-        }
-        .map { |rolled_column|
-              (0..rolled_column.length)
-                .filter_map {|i| i+1 if rolled_column[i] == "O"}
-                .reduce(&:+)
-            }
-        .reduce(&:+)
+    Platform
+      .new(input)
+      .tilt
+      .total_load_on_north_beams
   end
 
-  # example
-  #   day.part2 #=> 'how are you'
+  # example 1_000_000_000 cycles
+  #   day.part2 #=> 64
   def part2
   end
 
@@ -47,4 +28,73 @@ class Day14 < Day # >
     #....###..
     #OO..#....
   INPUT
+
+  FIRST_TILT_NORTH = <<~TILT
+    OOOO.#.O..
+    OO..#....#
+    OO..O##..O
+    O..#.OO...
+    ........#.
+    ..#....#.#
+    ..O..#.O.O
+    ..O.......
+    #....###..
+    #....#....
+  TILT
+end
+
+class Platform
+  require_relative 'grid'
+  require 'matrix'
+
+  ROUNDED = "O"
+  CUBED = "#"
+  EMPTY = "."
+
+  attr_reader :rounded_rocks, :immovable_rocks, :grid
+
+  def initialize(input)
+    @input = input
+    @grid = Grid.new(input).parse
+    @rows_max = @grid.row_bounds.max
+  end
+
+  # @example
+  #   p = new(Day14::EXAMPLE_INPUT)
+  #   p.tilt
+  #   p.to_s #=> Day14::FIRST_TILT_NORTH
+  def tilt
+    loop do
+      things_are_moving = false
+      rounded_rocks
+        .each do |coords, rock|
+          move_to = (Vector[*coords] + Vector[-1,0]).to_a
+          if grid.cover?(move_to) && grid.at(move_to) == EMPTY
+            grid.set(move_to, ROUNDED)
+            grid.set(coords, EMPTY)
+            things_are_moving = true
+          end
+        end
+      break unless things_are_moving
+    end
+    self
+  end
+
+  # @example
+  #   new(Day14::EXAMPLE_INPUT).tilt.total_load_on_north_beams #=> 136
+  def total_load_on_north_beams
+    rounded_rocks
+      .map { |coords, _value| @rows_max - coords[0] + 1 }
+      .reduce(&:+)
+  end
+
+  def rounded_rocks
+    grid.select { |_coords, value| value == ROUNDED }
+  end
+
+  # @example
+  #   new(Day14::EXAMPLE_INPUT).to_s #=> Day14::EXAMPLE_INPUT
+  def to_s
+    grid.to_s
+  end
 end

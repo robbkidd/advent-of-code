a0d4d501117c3f8ffd97d7651ed91e3ed918d145
Robb Kidd <robb@thekidds.og>
2023-12-15 07:47:18 -0500

    day 14, part 2 - wip
    Works for example input, but off on real input.

--- 2023/ruby/day14.rb
+++ 2023/ruby/day14.rb
@@ -7,13 +7,16 @@ class Day14 < Day # >
   def part1
     Platform
       .new(input)
-      .tilt
+      .tilt(:north)
       .total_load_on_north_beams
   end
 
-  # example 1_000_000_000 cycles
+  # @example 1_000_000_000 cycles
   #   day.part2 #=> 64
   def part2
+    p = Platform.new(input)
+    p.cycle(1_000_000_000)
+    p.total_load_on_north_beams
   end
 
   EXAMPLE_INPUT = <<~INPUT
@@ -41,6 +44,46 @@ class Day14 < Day # >
     #....###..
     #....#....
   TILT
+
+  AFTER_1_CYCLE = <<~CYCLE
+    .....#....
+    ....#...O#
+    ...OO##...
+    .OO#......
+    .....OOO#.
+    .O#...O#.#
+    ....O#....
+    ......OOOO
+    #...O###..
+    #..OO#....
+  CYCLE
+
+  AFTER_2_CYCLES = <<~CYCLE
+    .....#....
+    ....#...O#
+    .....##...
+    ..O#......
+    .....OOO#.
+    .O#...O#.#
+    ....O#...O
+    .......OOO
+    #..OO###..
+    #.OOO#...O
+  CYCLE
+
+  AFTER_3_CYCLES = <<~CYCLE
+    .....#....
+    ....#...O#
+    .....##...
+    ..O#......
+    .....OOO#.
+    .O#...O#.#
+    ....O#...O
+    .......OOO
+    #...O###.O
+    #.OOO#...O
+  CYCLE
+
 end
 
 class Platform
@@ -51,24 +94,65 @@ class Platform
   CUBED = "#"
   EMPTY = "."
 
-  attr_reader :rounded_rocks, :immovable_rocks, :grid
+  attr_reader :rounded_rocks, :immovable_rocks, :grid, :states
 
   def initialize(input)
     @input = input
+    @states = []
+    @loop_length = 0
     @grid = Grid.new(input).parse
     @rows_max = @grid.row_bounds.max
   end
 
-  # @example
+  # @example once
   #   p = new(Day14::EXAMPLE_INPUT)
-  #   p.tilt
+  #   p.cycle
+  #   p.to_s #=> Day14::AFTER_1_CYCLE
+  # @example twice
+  #   p = new(Day14::EXAMPLE_INPUT)
+  #   p.cycle(2)
+  #   p.to_s #=> Day14::AFTER_2_CYCLES
+  # @example thrice
+  #   p = new(Day14::EXAMPLE_INPUT)
+  #   p.cycle(3)
+  #   p.to_s #=> Day14::AFTER_3_CYCLES
+  def cycle(iterations = 1)
+    i = 0
+    while i < iterations do
+      [:north, :west, :south, :east].each { |direction| tilt(direction) }
+
+      current_state = rounded_rocks
+
+      if (seen_at = states.find_index(current_state)) && @loop_length == 0
+        @loop_length = i - seen_at
+        target_iter = (iterations - i) % @loop_length + 1
+        rounded_rocks.each { |coords| grid.set(coords, EMPTY) }
+        states[target_iter].each {|coords| grid.set(coords, ROUNDED) }
+        break [i, target_iter, total_load_on_north_beams]
+      else
+        states << current_state
+        i += 1
+      end
+    end
+  end
+
+  DIRECTIONS = {
+    north: Vector[-1, 0],
+    west:  Vector[ 0,-1],
+    south: Vector[ 1, 0],
+    east:  Vector[ 0, 1],
+  }
+
+  # @example 1_north
+  #   p = new(Day14::EXAMPLE_INPUT)
+  #   p.tilt(:north)
   #   p.to_s #=> Day14::FIRST_TILT_NORTH
-  def tilt
+  def tilt(direction)
     loop do
       things_are_moving = false
       rounded_rocks
-        .each do |coords, rock|
-          move_to = (Vector[*coords] + Vector[-1,0]).to_a
+        .each do |coords|
+          move_to = (Vector[*coords] + DIRECTIONS[direction]).to_a
           if grid.cover?(move_to) && grid.at(move_to) == EMPTY
             grid.set(move_to, ROUNDED)
             grid.set(coords, EMPTY)
@@ -81,15 +165,15 @@ class Platform
   end
 
   # @example
-  #   new(Day14::EXAMPLE_INPUT).tilt.total_load_on_north_beams #=> 136
+  #   new(Day14::EXAMPLE_INPUT).tilt(:north).total_load_on_north_beams #=> 136
   def total_load_on_north_beams
     rounded_rocks
-      .map { |coords, _value| @rows_max - coords[0] + 1 }
+      .map { |coords| @rows_max - coords[0] + 1 }
       .reduce(&:+)
   end
 
   def rounded_rocks
-    grid.select { |_coords, value| value == ROUNDED }
+    grid.filter_map { |coords, value| coords if value == ROUNDED }
   end
 
   # @example

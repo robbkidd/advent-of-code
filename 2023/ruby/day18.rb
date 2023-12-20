a29b718ced37ffbdce91be02092ab26c49a6d49c
Robb Kidd <robb@thekidds.og>
2023-12-19 21:44:53 -0500

    day 18 - only prepped

---
+++ 2023/ruby/day18.rb
@@ -0,0 +1,31 @@
+require_relative 'day'
+
+class Day18 < Day # >
+
+  # @example
+  #   day.part1 #=> 'hey'
+  def part1
+  end
+
+  # @example
+  #   day.part2 #=> 'how are you'
+  def part2
+  end
+
+  EXAMPLE_INPUT = <<~INPUT
+    R 6 (#70c710)
+    D 5 (#0dc571)
+    L 2 (#5713f0)
+    D 2 (#d2c081)
+    R 2 (#59c680)
+    D 2 (#411b91)
+    L 5 (#8ceee2)
+    U 2 (#caa173)
+    L 1 (#1b58a2)
+    U 2 (#caa171)
+    R 2 (#7807d2)
+    U 3 (#a77fa3)
+    L 2 (#015232)
+    U 2 (#7a21e3)
+  INPUT
+end

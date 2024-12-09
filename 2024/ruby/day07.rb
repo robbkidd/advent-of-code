f94c70d7d455ce2bef4a676a64905e48c43a7ce4
Robb Kidd <robb@thekidds.org>
2024-12-09 10:39:47 -0500

    day 7, part 1

---
+++ 2024/ruby/day07.rb
@@ -0,0 +1,33 @@
+require_relative 'day'
+
+class Day07 < Day # >
+
+  # @example
+  #   day.part1 #=> 3749
+  def part1
+    input
+      .split("\n")
+      .map { |line| line.scan(/\d+/).map(&:to_i) }
+      .map { |nums| [ nums[0] , nums[1..-1] ] }
+      .select { |test_value, operands|
+        operator_combos = [:+, :*].repeated_permutation(operands.length - 1)
+        operator_combos.map { |combo|
+          combo
+            .each_with_index
+            .reduce(operands[0]) { |result, (op, idx)|
+              result = [ result, operands[idx+1] ].reduce(op)
+            }
+        }
+        .any? { |result| result == test_value }
+      }
+      .map { |test_value, _| test_value }
+      .reduce(&:+)
+  end
+
+  # @example
+  #   day.part2 #=> 'how are you'
+  def part2
+  end
+
+  EXAMPLE_INPUT = File.read("../inputs/day07-example-input.txt")
+end

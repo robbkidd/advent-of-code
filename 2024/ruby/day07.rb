23e54030e47fabd9960c75d822f6d85f2fd485e6
Robb Kidd <robb@thekidds.org>
2024-12-09 10:43:51 -0500

    day 7, part 2
    Brute force! It's slow—39s for both parts—but it works.

--- 2024/ruby/day07.rb
+++ 2024/ruby/day07.rb
@@ -25,9 +25,34 @@ class Day07 < Day # >
   end
 
   # @example
-  #   day.part2 #=> 'how are you'
+  #   day.part2 #=> 11387
   def part2
+    input
+      .split("\n")
+      .map { |line| line.scan(/\d+/).map(&:to_i) }
+      .map { |nums| [ nums[0] , nums[1..-1] ] }
+      .select { |test_value, operands|
+        operator_combos = [:+, :*, :concat].repeated_permutation(operands.length - 1)
+        operator_combos.map { |combo|
+          combo
+            .each_with_index
+            .reduce(operands[0]) { |result, (op, idx)|
+              result = [ result, operands[idx+1] ].reduce(op)
+            }
+        }
+        .any? { |result| result == test_value }
+      }
+      .map { |test_value, _| test_value }
+      .reduce(&:+)
   end
 
   EXAMPLE_INPUT = File.read("../inputs/day07-example-input.txt")
+end
+
+class Integer
+  # @example
+  #   12.concat(345) #=> 12345
+  def concat(other)
+    "#{self}#{other}".to_i
+  end
 end

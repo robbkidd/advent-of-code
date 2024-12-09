14256da8225d9a1239d9d0c77af58933a8bdf459
Robb Kidd <robb@thekidds.org>
2024-12-09 11:32:33 -0500

    day 7, mmm objects
    Still slow (now 22s), but duplication removed

--- 2024/ruby/day07.rb
+++ 2024/ruby/day07.rb
@@ -6,50 +6,56 @@ class Day07 < Day # >
     @equations =
       input
         .split("\n")
-        .map { |line| line.scan(/\d+/).map(&:to_i) }
-        .map { |nums| [ nums[0] , nums[1..-1] ] }
+        .map { |line| CalibrationEquation.new(line) }
   end
 
   # @example
   #   day.part1 #=> 3749
   def part1
-    operators = [:+, :*]
     @equations
-      .select { |test_value, operands|
-        operators
-          .repeated_permutation(operands.length - 1)
-          .find { |combo|
-            test_value ==
-              combo
-                .each_with_index
-                .reduce(operands[0]) { |result, (op, idx)|
-                  result = [ result, operands[idx+1] ].reduce(op)
-                }
-          }
-      }
-      .map { |test_value, _| test_value }
+      .select{ _1.possibly_true([:+, :*]) }
+      .map(&:test_value)
       .reduce(&:+)
   end
 
   # @example
   #   day.part2 #=> 11387
   def part2
-    operators = [:+, :*, :concat]
     @equations
-      .select { |test_value, operands|
-        operators
-          .repeated_permutation(operands.length - 1)
-          .find { |combo|
-            test_value ==
-              combo
-                .each_with_index
-                .reduce(operands[0]) { |result, (op, idx)|
-                  result = [ result, operands[idx+1] ].reduce(op)
-                }
-          }
-      }
-      .map { |test_value, _| test_value }
+      .select{ _1.possibly_true([:+, :*, :concat]) }
+      .map(&:test_value)
       .reduce(&:+)
+  end
+
+  class CalibrationEquation
+    attr_reader :test_value, :operands
+
+    # @example
+    #   e = new("190: 10 19")
+    #   e.test_value #=> 190
+    #   e.operands   #=> [10, 19]
+    def initialize(input)
+      @test_value, *@operands =
+        input
+          .scan(/\d+/)
+          .map(&:to_i)
+    end
+
+    def possibly_true(operators)
+      operators
+        .repeated_permutation(operands.length - 1)
+        .find { |combo|
+          test_value == eval_with_operators(combo)
+        }
+    end
+
+    def eval_with_operators(operator_combo)
+      operator_combo
+        .each_with_index
+        .reduce(operands[0]) { |result, (op, idx)|
+          result = [ result, operands[idx+1] ].reduce(op)
+        }
+    end
   end
 
   EXAMPLE_INPUT = File.read("../inputs/day07-example-input.txt")

80b865ba577d8d57aa75dddad4535b4aed3599cb
Robb Kidd <robb@thekidds.org>
2025-12-06 11:09:00 -0500

    now with notes

--- 2025/ruby/day06.rb
+++ 2025/ruby/day06.rb
@@ -18,15 +18,15 @@ class Day06 < Day # >
   def part1
     input
       .split("\n")
-      .map {|line| line.scan(/\S+/)}
-      .cheater_transpose
+      .map {|line| line.scan(/\S+/)} # 2D of human numbers, last array is operators (all still strings)
+      .cheater_transpose             # transposed, now each array represents a worksheet problem (column)
       .map { |problem|
-        operator = problem.pop
+        operator = problem.pop       # problem's operator appears as the last element of the problem array
         problem
-          .map(&:to_i)
-          .reduce(&operator.to_sym)
+          .map(&:to_i)               # string of human number to integer
+          .reduce(&operator.to_sym)  # solve this problem
       }
-      .reduce(&:+)
+      .reduce(&:+)                   # sum of all problem solutions
   end
 
   # @example
@@ -34,18 +34,18 @@ class Day06 < Day # >
   def part2
     input
       .split("\n")
-      .map { |line| line.chars }
-      .cheater_transpose
-      .chunk { _1.all? " "} # mark empty columns as 'split'
-      .map { |split, problem|
-        next 0 if split
-        operator = problem.first.pop
+      .map { |line| line.chars }     # 2D of characters from the input
+      .cheater_transpose             # transposed, now each array is a column of characters from the worksheet
+      .chunk { |col| col.all? " "}   # group problem columns together by chunking on empties ...
+      .map { |split, problem|        # ... chunks that are empty are noted as split
+        next 0 if split              # skip the splits
+        operator = problem.first.pop # problem's operator appears as the last character in the first column
         problem
-          .map(&:join)
-          .map(&:to_i)
-          .reduce(&operator.to_sym)
+          .map(&:join)               # recreate a string of the digits (read vertically by the earlier transpose)
+          .map(&:to_i)               # make it a number (cephalopod number)
+          .reduce(&operator.to_sym)  # solve this problem
       }
-      .reduce(&:+)
+      .reduce(&:+)                   # sum of all problem solutions
   end
 
   EXAMPLE_INPUT = File.read("../inputs/day06-example-input.txt")
